007e2741082c02e824177a4be3bf365c07719fee
ktilak12 <tilakabhi0212@gmail.com>
2026-03-10 15:36:09 +0530

    renewed
    Updated welcome message and application information display. Removed user input and palindrome check logic.

--- PalindromeCheckerApp-feature-uc1/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp-feature-uc1/src/PalindromeCheckerApp.java
@@ -1,27 +1,18 @@
-import java.util.Scanner;
-public class PalindromeCheckerApp {
+// UC1: Display welcome message and application information
 
+public class PalindromeChecker {
+
+    // Main method - Entry point of the application
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
+        // Display welcome message
+        System.out.println("Welcome to the Palindrome Checker Application");
 
+        // Display application information
+        System.out.println("Application Name: PalindromeChecker");
+        System.out.println("Application Version: 1.0");
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a word:");
-        String word = scanner.nextLine();
-
-        System.out.println("You entered: " + word);
-
-        String reversed = new StringBuilder(word).reverse().toString();
-
-        if(word.equalsIgnoreCase(reversed)) {
-            System.out.println("It is a palindrome");
-        } else {
-            System.out.println("It is NOT a palindrome");
-        }
-
+        // Message indicating program will continue
+        System.out.println("Program is ready to check palindromes...");
     }
-
 }
