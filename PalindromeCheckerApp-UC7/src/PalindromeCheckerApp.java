7a80da7e4f20ec5006e60a234a3154f39a7f9bdf
ktilak12 <tilakabhi0212@gmail.com>
2026-03-10 15:44:52 +0530

    Implement palindrome check using Deque
    Refactored PalindromeCheckerApp to use Deque for palindrome checking instead of reversing the string.

--- PalindromeCheckerApp-UC7/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp-UC7/src/PalindromeCheckerApp.java
@@ -1,27 +1,44 @@
-import java.util.Scanner;
-public class PalindromeCheckerApp {
+```java
+// UC7: Use Deque to compare front and rear elements
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class PalindromeChecker {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
+        // Original string
+        String word = "level";
 
+        // Create a Deque
+        Deque<Character> deque = new LinkedList<>();
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a word:");
-        String word = scanner.nextLine();
-
-        System.out.println("You entered: " + word);
-
-        String reversed = new StringBuilder(word).reverse().toString();
-
-        if(word.equalsIgnoreCase(reversed)) {
-            System.out.println("It is a palindrome");
-        } else {
-            System.out.println("It is NOT a palindrome");
+        // Insert characters into the deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
         }
 
-    }
+        boolean isPalindrome = true;
 
+        // Compare front and rear characters
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
 }
+```
